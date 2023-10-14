9fd5ba40d0abc1c7bddb93f0504addb613c9f982
king-midas <mto0006@auburn.edu>
2023-10-14 18:13:42 -0500

    Update NavBar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -4,11 +4,11 @@ import { Typography, Sheet, Link } from '@mui/joy';
 const NavigationBar = () => {
   return (
     <Sheet sx={{ boxShadow: 'rgba(0, 0, 0, 0.05) 0px 0px 0px 1px', padding: '10px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-      <Typography variant="h6" fontWeight="bold" fontSize="15">
+      <Typography fontWeight="bold" fontSize="15">
         BlockExplorer
       </Typography>
 
-      <Link href="/explore" color="inherit" underline="none" fontWeight="bold" fontSize="12">
+      <Link href="/Explore" underline="none" fontWeight="bold" fontSize="12">
         Explore
       </Link>
     </Sheet>
