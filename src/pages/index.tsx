c0c8fe7c69471a36be6707e69494be3b081d368e
Elijah Hampton <elijahhampton@MBP-de-Elijah-2.lan>
2023-10-14 14:17:58 -0400

    Implements search bar component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,8 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '../styles/Home.module.css'
-import Table from '../components/Table'
-import { Typography, Box, Avatar, Stack } from '@mui/joy'
+import { Stack } from '@mui/joy'
 import React from 'react'
-
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import SearchIcon from '@mui/icons-material/Search';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
-import BlockIcon from '@mui/icons-material/Block';
-import AutorenewRoundedIcon from '@mui/icons-material/AutorenewRounded';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import MoreHorizRoundedIcon from '@mui/icons-material/MoreHorizRounded';
-import { ColorPaletteProp } from '@mui/joy/styles';
 import BlockTable from '../containers/BlockTable'
 
 const inter = Inter({ subsets: ['latin'] })
